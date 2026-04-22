93666e131d93866a7c0544aff857bda8cc408882
rambiddamm-stack <rambiddamm@gmail.com>
2026-04-22 14:22:46 +0500

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
-import { signUp } from '../lib/supabase'
+import { signUp } from '../lib/firebase'
 import { useToast } from '../components/Layout'
 
 export default function SignupPage() {
